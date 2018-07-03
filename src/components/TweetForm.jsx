a73d13b834059bfe1603cb8f858586d1f456eaba
Joe McKinney and Robin Sola <robin@>
2018-07-03 10:18:54 -0700

    add refs for form inputs

--- src/components/TweetForm.jsx
+++ src/components/TweetForm.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
 function TweetForm() {
+  let _title = null;
+  let _tweetBody = null;
+
+
+  function handleNewTweetFormSubmission(event) {
+    event.preventDefault()
+    console.log(_title.value)
+    console.log(_tweetBody.value)
+    _title.value = ''
+    _tweetBody.value = ''
+  }
+
   return(
-    <div>
+    <div onSubmit={handleNewTweetFormSubmission}>
       <form>
         <input
           type='text'
           id='title'
           placeholder='Name Your Tweet'
-          />
+          ref= {(input) => {_title = input}} />
         <br/>
         <br />
         <textarea
           id="tweetBody"
-          placeholder="Gimme the tweet" />
+          placeholder="Gimme the tweet"
+          ref= {(input) => {_tweetBody = input}} />
         <br />
         <button type="submit">Tweet Thah</button>
       </form>
